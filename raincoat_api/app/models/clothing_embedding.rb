ae741177ec66e40c96c59023810bbd33b1a6d67a
Rudy Guerrero <115583183+rGuerrero91@users.noreply.github.com>
2025-09-19 16:32:00 -0400

    Update clothing_embedding.rb

--- raincoat_api/app/models/clothing_embedding.rb
+++ raincoat_api/app/models/clothing_embedding.rb
@@ -1,2 +1,9 @@
+# app/models/clothing_embedding.rb
 class ClothingEmbedding < ApplicationRecord
+  belongs_to :clothing_piece
+  
+  validates :clothing_piece_id, uniqueness: { scope: :model_version }
+  validates :vector_data, presence: true
+  validates :model_version, presence: true
+  
 end
