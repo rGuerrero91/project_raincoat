6e63f60f81fa49ff337b864bcafb23661edb9fd0
Rudy Guerrero <115583183+rGuerrero91@users.noreply.github.com>
2025-09-20 18:41:53 -0400

    logout route fixed

--- raincoat_api/config/routes.rb
+++ raincoat_api/config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
   
   # Main wardrobe interface (this creates clothing_pieces_path)
   resources :clothing_pieces, path: 'wardrobe' do
