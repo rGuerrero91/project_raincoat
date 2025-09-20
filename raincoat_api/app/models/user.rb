8ff4a87e0064dfec4a4c5a23c78ac443f82e38f8
Rudy Guerrero <115583183+rGuerrero91@users.noreply.github.com>
2025-09-19 23:07:57 -0400

    update user model for signup

--- raincoat_api/app/models/user.rb
+++ raincoat_api/app/models/user.rb
@@ -1,2 +1,15 @@
+# app/models/user.rb
 class User < ApplicationRecord
+  has_many :clothing_pieces, dependent: :destroy
+  
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true
+  
+  before_save :normalize_email
+  
+  private
+  
+  def normalize_email
+    self.email = email.downcase.strip if email.present?
+  end
 end
