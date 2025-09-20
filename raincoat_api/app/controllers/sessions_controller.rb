f0877b7f24023f93d438326d7fa6a09e090a4b06
Rudy Guerrero <115583183+rGuerrero91@users.noreply.github.com>
2025-09-19 23:06:28 -0400

    more sessions control

---
+++ raincoat_api/app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+# app/controllers/sessions_controller.rb
+class SessionsController < ApplicationController
+  def new
+    redirect_to clothing_pieces_path if current_user
+  end
+  
+  def create
+    user = User.find_by(email: params[:email].downcase.strip)
+    
+    if user
+      session[:user_id] = user.id
+      redirect_to clothing_pieces_path, notice: 'Logged in successfully!'
+    else
+      flash.now[:alert] = 'Invalid email address. Please try again or create an account.'
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: 'Logged out successfully!'
+  end
+end
---
+++ raincoat_api/app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+# app/controllers/users_controller.rb
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to cllothing_pieces_path, notice: 'Account created successfully!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:email, :name)
+  end
+end
