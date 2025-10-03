04bfd0f9b586e8875ef5b0bbea2e933710261583
Rudy Guerrero <115583183+rGuerrero91@users.noreply.github.com>
2025-10-02 22:58:56 -0400

    Update schema.rb

--- raincoat_api/db/schema.rb
+++ raincoat_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_25_113850) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_03_022012) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "vector"
@@ -47,7 +47,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_25_113850) do
     t.bigint "clothing_piece_id", null: false
     t.vector "vector_data", limit: 512, null: false
     t.string "model_version", default: "tinyclip-1.0", null: false
-    t.float "confidence_score"
     t.json "preprocessing_metadata"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
