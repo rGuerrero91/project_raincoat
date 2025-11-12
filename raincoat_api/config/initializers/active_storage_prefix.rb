c3da5b271f1d597bd7dce35bc82f4dbf7e0bddf5
Rudy Guerrero <115583183+rGuerrero91@users.noreply.github.com>
2025-11-11 20:45:15 -0500

    Create initializer for Active Storage prefix
    Add custom prefix for Active Storage key generation

---
+++ raincoat_api/config/initializers/active_storage_prefix.rb
@@ -0,0 +1,19 @@
+# This initializer intercepts the key generation to add a custom prefix.
+
+ActiveSupport.on_load(:active_storage_blob) do
+  # Define the desired prefix, perhaps using an environment variable
+  # Ensure the prefix matches what you had in storage.yml
+  S3_PREFIX = ENV['S3_PREFIX'] || 'uploads/clothing'
+
+  if S3_PREFIX.present?
+    # Override the default key method to prepend the prefix
+    def key
+      # Only prepend the prefix if the key hasn't been set yet (for new records)
+      if self[:key].nil?
+        self[:key] = File.join(S3_PREFIX, self.class.generate_unique_secure_token)
+      end
+      self[:key]
+    end
+  end
+end
+
