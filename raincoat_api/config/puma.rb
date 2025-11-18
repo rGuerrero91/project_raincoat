379cfee1fff44e0228fdebdda4dd6d6703800f8a
Rudy Guerrero <r.guerreronyc@gmail.com>
2025-11-18 02:44:14 -0500

    Update .gitignore

--- raincoat_api/config/puma.rb
+++ raincoat_api/config/puma.rb
@@ -30,9 +30,6 @@ threads threads_count, threads_count
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
 
-# Bind to 0.0.0.0 to allow access from other devices on the network
-bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 3000)}"
-
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
