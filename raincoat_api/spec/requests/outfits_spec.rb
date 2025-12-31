cc84acb4c0d01f183f3d4225c0276427f50bbc2e
Rudy Guerrero <r.guerreronyc@gmail.com>
2025-12-31 15:49:12 -0500

    fix test routes

--- raincoat_api/spec/requests/outfits_spec.rb
+++ raincoat_api/spec/requests/outfits_spec.rb
@@ -1,30 +1,39 @@
 require 'rails_helper'
 
-RSpec.describe "Outfits", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/outfits/index"
+RSpec.describe 'Outfits', type: :request do
+  let(:user) { User.create!(email: 'test@example.com') }
+  let(:location) { Location.create!(city: 'New York', country: 'US', latitude: 40.7128, longitude: -74.0060, user: user) }
+
+  describe 'GET /api/v1/outfits' do
+    it 'returns http success' do
+      get '/api/v1/outfits', params: { user_id: user.id }
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/outfits/create"
+  describe 'POST /api/v1/outfits' do
+    it 'returns http success' do
+      post '/api/v1/outfits', params: {
+        user_id: user.id,
+        location_id: location.id,
+        outfit_items: []
+      }
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/outfits/show"
+  describe 'GET /api/v1/outfits/:id' do
+    it 'returns http success' do
+      outfit = Outfit.create!(user: user, location: location)
+      get "/api/v1/outfits/#{outfit.id}"
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/outfits/destroy"
+  describe 'DELETE /api/v1/outfits/:id' do
+    it 'returns http success' do
+      outfit = Outfit.create!(user: user, location: location)
+      delete "/api/v1/outfits/#{outfit.id}"
       expect(response).to have_http_status(:success)
     end
   end
