97a53f6cd6364d9de0f5077ac179d6eca9811b0d
Rudy Guerrero <r.guerreronyc@gmail.com>
2026-01-08 01:42:17 -0500

    Update jest.config.js

--- raincoat_frontend/jest.config.js
+++ raincoat_frontend/jest.config.js
@@ -7,7 +7,7 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
